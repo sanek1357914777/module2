549386ab850c129b51df1c6a68b1ecb2c2d8a8fd
Aleksandr <sanek1357914@gmail.com>
2019-06-15 13:51:20 +0300

    wtf2

--- src/ElementCounter.java
+++ src/ElementCounter.java
@@ -1,10 +1,28 @@
-import java.util.Map;
+import java.util.*;
 
 public class ElementCounter {
-    private String string;
-    private int count;
 
-   void counter(String stringIn){
-         this.string= stringIn;
+    static void counter(String string) {
+        HashSet<Character> in = new HashSet<Character>();
+        char[] simbols = string.toCharArray();
+        System.out.println(Arrays.toString(simbols));
+        int k = 0;
+        int targetChar = 0;
+
+        for (char simbol : simbols) {
+            if (!in.contains(simbol)) {
+                k = 1;
+                for (int i = targetChar + 1; i < simbols.length; i++) {
+                    if ((simbols[targetChar] - simbols[i] == 0)) {
+                        k++;
+                    }
+                }
+                System.out.println(k);
+                in.add(simbol);
+                targetChar++;
+            }
+            System.out.println(in);
+
+        }
     }
 }
